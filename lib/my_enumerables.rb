3020c237cb3d903856cd307d967be6c79594ea87
CodeByBlazej <codebyblazej@gmail.com>
2024-10-26 08:47:10 +0100

    solve each with index exercise

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,10 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    self.each_with_index do |elem, index|
+      elem if yield(elem, index)
+    end
+  end
 end
 
 # You will first have to define my_each
